d29e4109091a52a3faf74bedd7385cd9f1a0e1bc
Enes <ereseres1579@gmail.com>
2023-11-10 23:49:35 +0300

    :octocat:

---
+++ _ JAVA _ Homeworks/LAB 6 four constructor/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Person enes = new Person("enes","celebi",12);  //butun parametreler
+		System.out.println(enes.toString());
+		Person omer = new Person("omer","guney"); // sadece isim ve soyisim
+		System.out.println(omer.toString());
+	}
+}
+
+
---
+++ _ JAVA _ Homeworks/LAB 6 four constructor/Person.java
@@ -0,0 +1,56 @@
+
+public class Person {
+	private String name,lastName;
+	public static int id = 1;
+	private int age;
+	
+
+	
+	Person(String name,String lastname,int age){
+		setName(name);
+		setLastName(lastname);
+		setId(id);
+		setAge(age);
+		id++;
+	}
+	Person(String name,String lastname){
+		this(name,lastname,20);
+	}
+	Person(int id){
+		this("default ","default ",age);
+	}
+	Person(int id){
+		this("default ","default ",age);
+	}
+	
+	public String toString() {
+		return ("Adı: "+name+" "+lastName+" id: "+id+" Yaşı: "+age);
+	}
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public static int getId() {
+		return id;
+	}
+	public static void setId(int id) {
+		Person.id = id;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
